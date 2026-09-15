ee6620807f213589c1510ac124fbef128b5f74dc
Relayced <ralphdenverdimapilis@gmail.com>
2026-09-15 11:37:49 +0800

    Release: configure launcher default repository to relayced/Hexagon for v1.4.4

--- nefhub.sh
+++ nefhub.sh
@@ -2,7 +2,7 @@
 # Nefarious Hub Termux Launcher v1.4.4
 set -e
 
-REPO="${NEF_REPO:-relayced/test-nef}"
+REPO="${NEF_REPO:-relayced/Hexagon}"
 BRANCH="main"
 BASE_URL="https://raw.githubusercontent.com/$REPO/$BRANCH"
 VERSION_URL="$BASE_URL/version.txt"
